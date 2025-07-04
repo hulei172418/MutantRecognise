bbd3355eaaf218dd02ed988d40772f27edac9c71
hulei172418 <390420686@qq.com>
2025-07-04 15:57:48 +0800

    12

---
+++ testset/commons-lang3-3.17.0/result/main.java.org.apache.commons.lang3.ArrayFill/traditional_mutants/float[]_fill(float[],float)/SDL_13/ArrayFill.java
@@ -0,0 +1,80 @@
+// This is a mutant program.
+// Author : ysma
+
+package org.apache.commons.lang3;
+
+
+import java.util.Arrays;
+
+
+public final class ArrayFill
+{
+
+    public static  byte[] fill( final byte[] a, final byte val )
+    {
+        if (a != null) {
+            Arrays.fill( a, val );
+        }
+        return a;
+    }
+
+    public static  char[] fill( final char[] a, final char val )
+    {
+        if (a != null) {
+            Arrays.fill( a, val );
+        }
+        return a;
+    }
+
+    public static  double[] fill( final double[] a, final double val )
+    {
+        if (a != null) {
+            Arrays.fill( a, val );
+        }
+        return a;
+    }
+
+    public static  float[] fill( final float[] a, final float val )
+    {
+        if (a != null) {
+        }
+        return a;
+    }
+
+    public static  int[] fill( final int[] a, final int val )
+    {
+        if (a != null) {
+            Arrays.fill( a, val );
+        }
+        return a;
+    }
+
+    public static  long[] fill( final long[] a, final long val )
+    {
+        if (a != null) {
+            Arrays.fill( a, val );
+        }
+        return a;
+    }
+
+    public static  short[] fill( final short[] a, final short val )
+    {
+        if (a != null) {
+            Arrays.fill( a, val );
+        }
+        return a;
+    }
+
+    public static <T> T[] fill( final T[] a, final T val )
+    {
+        if (a != null) {
+            Arrays.fill( a, val );
+        }
+        return a;
+    }
+
+    private ArrayFill()
+    {
+    }
+
+}
